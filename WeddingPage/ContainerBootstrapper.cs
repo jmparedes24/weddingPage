8b0fad8548c953e288308f80bbb5a744fe114ffa
Juan Paredes <jmpl240187@gmail.com>
2015-12-02 23:09:36 -0600

    Adding Bootstrapper

---
+++ WeddingPage/ContainerBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeddingPage
+{
+    public class ContainerBootstrapper
+    {
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            // NOTE: To load from web.config uncomment the line below...
+            // container.LoadConfiguration();
+
+            // TODO: Register your types here
+            // container.RegisterType<IProductRepository, ProductRepository>();
+        }
+    }
+}
+}
