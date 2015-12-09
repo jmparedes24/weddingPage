dbb2d0c0dab5bcd209cbfa29dbf8e147465b8319
Juan Paredes <jmpl240187@gmail.com>
2015-12-08 21:06:17 -0600

    Adding layout configuration

---
+++ WeddingPage.Sql.Finance.Data.Test/StatusReposuitoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace WeddingPage.Sql.Finance.Data.Test
+{
+    [TestFixture]
+    public class StatusReposuitoryTest
+    {
+        [SetUp]
+        public void TestMethod1()
+        {
+        }
+    }
+}
